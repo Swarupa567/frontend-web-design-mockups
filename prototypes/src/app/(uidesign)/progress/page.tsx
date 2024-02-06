476534fbcb88db7edb10943d4c3ee91cd4144b01
Swarupa567 <swarupachowdaryp@gmail.com>
2024-02-06 17:01:09 +0530

    feat: added progressCard component

---
+++ prototypes/src/app/(uidesign)/progress/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import ProgressCard from '@/components/pageComponents/progressCard';
+
+function Progress() {
+  return (
+    <div>
+        <ProgressCard />
+    </div>
+  )
+}
+
+export default Progress;
---
+++ prototypes/src/components/pageComponents/progressCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Card,CardHeader, CardTitle, CardDescription,CardContent, CardFooter} from '@/components/ui/card';
+
+const ProgressCard = () => {
+  return (
+    <div className='items-center mt-22 text-[#000] text-md'>
+        <Card className='bg-[#D1D0F5] text-center w-[29%] flex flex-col justify-center items-center m-auto right-[30rem] absolute top-0 mt-20 p-[90px]'>
+            <CardHeader>
+            </CardHeader>
+        </Card>
+        <Card className='text-center w-[27%] flex flex-col justify-center items-start m-auto relative mt-[8rem] p-[39px]'>
+            <CardHeader>
+                <CardTitle className='items-start font-roboto'>Your Progress</CardTitle>
+            </CardHeader>
+            <CardContent className=" pl-8 font-roboto">
+                <CardDescription className='flex justify-between items-start text-[#000]'>
+                    <div className='w-16 h-16 border-4 border-solid border-[#D5D4F0] rounded-full flex items-center justify-center font-bold'>93%</div>
+                    <div className='pl-[18px] pt-[12px]'>
+                    <p className='font-semibold'>32 of 42 complete</p>
+                    <p>Finish course</p>
+                    </div>
+               
+                    </CardDescription>
+            </CardContent>
+        </Card>
+    </div>
+  );
+};
+
+export default ProgressCard;
